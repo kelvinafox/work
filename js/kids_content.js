a6d9d5e370ad1c0660feecc4741748a1ee34284b
kelvinafox <kelvin.a.fox@gmail.com>
2015-11-16 10:28:05 -0800

    Original Commit

---
+++ js/kids_content.js
@@ -0,0 +1,72 @@
+var num_items_giant = 8;
+
+var bike_pics_giant = ["pictures/bikes/2016/kids/XTC-Jr-24/XtC-SL-Jr-24-Dark-Blue.jpg",
+				 "pictures/bikes/2016/kids/XTC-Jr-24/XtC-Jr-1-Disc-24-Orange.jpg",
+				 "pictures/bikes/2016/kids/XTC-Jr-24/XtC-Jr-24-Lite-Blue.jpg",
+				 "pictures/bikes/2016/kids/XTC-Jr-20/XtC-Jr-20-Lite-Green.jpg",
+				 "pictures/bikes/2016/kids/Animator-16/Animator-C-B-16-Orange.jpg",
+				 "pictures/bikes/2016/kids/Animator-12/Animator-C-B-12-Black.jpg", 
+				 "pictures/bikes/2016/kids/Amplify/Amplify-CB.jpg",
+				 "pictures/bikes/2016/kids/Pre/Pre-Boys-Red.jpg"];
+
+var bike_titles_giant = ["Giant XtC SL Jr 24",
+				   "Giant XtC Jr Disc 24",
+				   "Giant XtC Jr 24 Lite",
+				   "Giant XtC Jr 20 Lite",
+				   "Giant Animator 16",
+				   "Giant Animator 12",
+				   "Giant Amplify",
+				   "Giant Pre"];
+
+var bike_descriptions_giant = ["The offspring of a well-loved Giant hardtail. The perfect blend of preformance features and dialed-in fit for young riders.<br><strong>$650</strong>",
+						 "Big-time off-road features in a mini package. With front suspension and disc brakes, he's ready for big boy terrain.<br><strong>$400</strong>",
+						 "Light and ready for dirt, a perfect into into off-road riding.<br><strong>$255</strong>",
+						 "Light and ready for dirt, a perfect into into off-road riding.<br><strong>$235</strong>",
+						 "The cycling life starts here. Animator makes it easy to get rolling, and it's built tough for neighborhood adventures.<br><strong>$180</strong>",
+						 "The cycling life starts here. Animator makes it easy to get rolling, and it's built tough for neighborhood adventures.<br><strong>$170</strong>",
+						 "The fun doesn't have to end when a young rider grows taller. With its innovative grow technology, Amplify lasts and lasts.<br><strong>$240</strong>",
+						 "Learning to balance and ride a bike has never been easier or more fun. Pre is the start of something beautiful.<br><strong>$130</strong>"];
+
+var bike_modal_descriptions_giant = ["This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+				   		        "This is a test modal body"];
+
+var num_items_liv = 7;
+
+var bike_pics_liv = ["pictures/bikes/2016/kids/Enchant-24/Enchant-1-24-Purple.jpg",
+				 	 "pictures/bikes/2016/kids/Enchant-24-Lite/Enchant-24-Lite-White.jpg",
+				 	 "pictures/bikes/2016/kids/Enchant-20/Enchant-20-Lite-Blue.jpg",
+				 	 "pictures/bikes/2016/kids/Bella/Bella-Aqua.jpg",
+				 	 "pictures/bikes/2016/kids/Adore/Adore-CB-16-White.jpg",
+				 	 "pictures/bikes/2016/kids/Adore/Adore-CB-12-Pink.jpg",
+				 	 "pictures/bikes/2016/kids/Blossom/Blossom-CB-Profile-purple-medium.jpg"];
+
+var bike_titles_liv = ["Liv Enchant 24",
+					   "Liv Enchant 24 Lite",
+				  	   "Liv Enchant 20 Lite",
+				  	   "Live Bella 20",
+				  	   "Liv Adore 16",
+					   "Liv Adore 12",
+					   "Liv Blossom"];
+
+var bike_descriptions_liv = ["What makes trail riding fun for kids? It starts with the right bike. Enchant ensures smooth sailing, even on bumpy trails.<br><strong>$340</strong>",
+							 "Lightweight and easy to maneuver, Enchant Lite puts her on a path to adventure.<br><strong>$255</strong>",
+						 	 "Lightweight and easy to maneuver, Enchant Lite puts her on a path to adventure.<br><strong>$235</strong>",
+							 "Spark her sense of adventure. Bella is built to ignite a love for cycling and ourdoor activities.<br><strong>$210</strong>",
+							 "Her first two-wheeler. One of life's greatest experiences. Stable, safe, and fun, The colorful Adore makes it easy.<br><strong>$190</strong>",
+							 "Her first two-wheeler. One of life's greatest experiences. Stable, safe, and fun, The colorful Adore makes it easy.<br><strong>$180</strong>",
+							 "Young ones grow up, and Blossom grows with them. Colors she'll love, and a lasting design that you'll love too.<br><strong>$240</strong>"];
+
+var bike_modal_descriptions_liv = ["This is a test modal body",
+						    	   "This is a test modal body",
+						           "This is a test modal body",
+						    	   "This is a test modal body",
+						           "This is a test modal body",
+						           "This is a test modal body",
+						    	   "This is a test modal body",
+				   		           "This is a test modal body"];
---
+++ js/road_content.js
@@ -0,0 +1,153 @@
+// Giant Brand
+var num_items_giant = 9;
+
+var bike_pics_giant = ["pictures/bikes/2016/mtb/Anthem/Anthem-X-29er-Black.jpg",
+				 "pictures/bikes/2016/mtb/Trance/Trance-27.5-2/Trance-275-2-Charcoal.jpg",
+				 "pictures/bikes/2016/mtb/Trance/Trance-27.5-3/Trance-275-3-Black.jpg",
+				 "pictures/bikes/2016/mtb/Stance/Stance-27.5-2/Stance-275-2-Black.jpg",
+				 "pictures/bikes/2016/mtb/Talon/Talon-27.5-1/Talon-275-1-Black-Green.jpg",
+				 "pictures/bikes/2016/mtb/Talon/Talon-27.5-2/Talon-275-2-Silver-Blue.jpg", 
+				 "pictures/bikes/2016/mtb/Talon/Talon-27.5-3/Talon-27-5-3-Charcoal.jpg",
+				 "pictures/bikes/2016/mtb/ATX/ATX-275-2-Black-Red.jpg", 
+				 "pictures/bikes/2016/mtb/Revel/Revel-2/Revel-2-Black.jpg"];
+
+var bike_titles_giant = ["Giant Anthem X 29er",
+				   "Giant Trance 27.5 2",
+				   "Giant Trance 27.5 3",
+				   "Giant Stance 27.5 2",
+				   "Giant Talon 27.5 1",
+				   "Giant Talon 27.5 2",
+				   "Giant Talon 27.5 3",
+				   "Giant ATX 27.5 2",
+				   "Giant Revel 2"];
+
+var bike_descriptions_giant = ["Smooth, stable and in control. Crush technical terrain with this confident XC roller.<br><strong>$2700</strong>",
+						 "More control on steep, aggressive terrain, power and speed on technical climbs. Pure confidence for every type of trail.<br><strong>$2700</strong>",
+						 "More control on steep, aggressive terrain, power and speed on technical climbs. Pure confidence for every type of trail.<br><strong>$2100</strong>",
+						 "When the trail calls, Stance 27.5 answers. Confident, quick and fun to ride, it opens up a world of off-road possibilities.<br><strong>$1500</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$1400</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$1100</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$700</strong>",
+						 "On-road or off-road, the choice is yours. With smooth, confident 27.5 wheels, this is your invitation to explore.<br><strong>$460</strong>",
+						 "Take a spin on the bike path or head into the forest. For aspiring off-road riders, revel lets you blaze new trails.<br><strong>$370</strong>"];
+
+var bike_modal_titles_giant = ["Giant Anthem X 29er",
+				   		  "Giant Trance 27.5 2",
+				   		  "Giant Trance 27.5 3",
+						  "Giant Stance 27.5 2",
+						  "Giant Talon 27.5 1",
+						  "Giant Talon 27.5 2",
+						  "Giant Talon 27.5 3",
+						  "Giant ATX 27.5 2",
+						  "Giant Revel 2"];
+
+var bike_modal_descriptions_giant = ["This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+				   		        "This is a test modal body",
+				   		        "This is a test modal body"];
+
+// Liv Brand
+var num_items_liv = 8;
+
+var bike_pics_liv = ["pictures/bikes/2016/mtb/Lust/Lust-1/Lust-1-Blue.jpg",
+					 "pictures/bikes/2016/mtb/Lust/Lust-2/Lust-2-Black-Pink.jpg",
+					 "pictures/bikes/2016/mtb/Tempt/Tempt-1/Tempt-1-Black.jpg",
+					 "pictures/bikes/2016/mtb/Tempt/Tempt-2/Tempt-2-White.jpg",
+					 "pictures/bikes/2016/mtb/Tempt/Tempt-3/Tempt-3-Dark-Green.jpg",
+					 "pictures/bikes/2016/mtb/Tempt/Tempt-4/Tempt-4-Black.jpg",
+					 "pictures/bikes/2016/mtb/Enchant/Enchant-1/Enchant-1-Black-Purple.jpg",
+					 "pictures/bikes/2016/mtb/Enchant/Enchant-2/Enchant-2-White-Blue.jpg"];
+
+var bike_titles_liv = ["Liv Lust 1",
+					   "Liv Lust 2",
+					   "Liv Tempt 1",
+					   "Liv Tempt 2",
+					   "Liv Tempt 3",
+					   "Liv Tempt 4",
+					   "Liv Enchant 1",
+					   "Liv Enchant 2"];
+
+var bike_descriptions_liv = ["The trifecta of less weight, more control, and increased efficiency equals a fun and confident XC bike.<br><strong>$2900</strong>",
+							 "The trifecta of less weight, more control, and increased efficiency equals a fun and confident XC bike.<br><strong>$2500</strong>",
+							 "Make your first foray into the dirt or graduate to advanced singletrack. Tempt is designed to be with you all the way.<br><strong>$1400</strong>",
+							 "Make your first foray into the dirt or graduate to advanced singletrack. Tempt is designed to be with you all the way.<br><strong>$1100</strong>",
+							 "Make your first foray into the dirt or graduate to advanced singletrack. Tempt is designed to be with you all the way.<br><strong>$700</strong>",
+							 "Make your first foray into the dirt or graduate to advanced singletrack. Tempt is designed to be with you all the way.<br><strong>$600</strong>",
+							 "Become enchanted with cycling on roads, paths or dirt. The versatile and capable enchant.<br><strong>$430</strong>",
+							 "Become enchanted with cycling on roads, paths or dirt. The versatile and capable enchant.<br><strong>$370</strong>"];
+
+var bike_modal_titles_liv = ["Live Lust 1",
+				   			 "Liv Lust 2",
+				   			 "Liv Tempt 1",
+							 "Liv Tempt  2",
+							 "Liv Tempt 3",
+							 "Liv Tempt 4",
+							 "Liv Enchant 1",
+							 "Liv Enchant 2"];
+
+var bike_modal_descriptions_liv = ["This is a test modal body",
+						    	   "This is a test modal body",
+						           "This is a test modal body",
+						    	   "This is a test modal body",
+						    	   "This is a test modal body",
+						           "This is a test modal body",
+						           "This is a test modal body",
+						           "THis is a test bodal body"];
+
+// Kona Brand
+var num_items_kona = 4;
+
+var bike_pics_kona = ["pictures/bikes/2016/mtb/Anthem/Anthem-X-29er-Black.jpg",
+					  "pictures/bikes/2016/mtb/Trance/Trance-27.5-2/Trance-275-2-Charcoal.jpg",
+					  "pictures/bikes/2016/mtb/Trance/Trance-27.5-3/Trance-275-3-Black.jpg",
+					  "pictures/bikes/2016/mtb/Stance/Stance-27.5-2/Stance-275-2-Black.jpg",
+					  "pictures/bikes/2016/mtb/Talon/Talon-27.5-1/Talon-275-1-Black-Green.jpg",
+					  "pictures/bikes/2016/mtb/Talon/Talon-27.5-2/Talon-275-2-Silver-Blue.jpg", 
+					  "pictures/bikes/2016/mtb/Talon/Talon-27.5-3/Talon-27-5-3-Charcoal.jpg",
+					  "pictures/bikes/2016/mtb/ATX/ATX-275-2-Black-Red.jpg", 
+					  "pictures/bikes/2016/mtb/Revel/Revel-2/Revel-2-Black.jpg"];
+
+var bike_titles_kona = ["Giant Anthem X 29er",
+				   "Giant Trance 27.5 2",
+				   "Giant Trance 27.5 3",
+				   "Giant Stance 27.5 2",
+				   "Giant Talon 27.5 1",
+				   "Giant Talon 27.5 2",
+				   "Giant Talon 27.5 3",
+				   "Giant ATX 27.5 2",
+				   "Giant Revel 2"];
+
+var bike_descriptions_kona = ["Smooth, stable and in control. Crush technical terrain with this confident XC roller.<br><strong>$2700</strong>",
+						 "More control on steep, aggressive terrain, power and speed on technical climbs. Pure confidence for every type of trail.<br><strong>$2700</strong>",
+						 "More control on steep, aggressive terrain, power and speed on technical climbs. Pure confidence for every type of trail.<br><strong>$2100</strong>",
+						 "When the trail calls, Stance 27.5 answers. Confident, quick and fun to ride, it opens up a world of off-road possibilities.<br><strong>$1500</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$1400</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$1100</strong>",
+						 "Crank up tricky climbs and carve through corners. This balanced 27.5 hardtail lets you hit the trail with confidence.<br><strong>$700</strong>",
+						 "On-road or off-road, the choice is yours. With smooth, confident 27.5 wheels, this is your invitation to explore.<br><strong>$460</strong>",
+						 "Take a spin on the bike path or head into the forest. For aspiring off-road riders, revel lets you blaze new trails.<br><strong>$370</strong>"];
+
+var bike_modal_titles_kona = ["Giant Anthem X 29er",
+				   		  "Giant Trance 27.5 2",
+				   		  "Giant Trance 27.5 3",
+						  "Giant Stance 27.5 2",
+						  "Giant Talon 27.5 1",
+						  "Giant Talon 27.5 2",
+						  "Giant Talon 27.5 3",
+						  "Giant ATX 27.5 2",
+						  "Giant Revel 2"];
+
+var bike_modal_descriptions_kona = ["This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+						        "This is a test modal body",
+						        "This is a test modal body",
+						       "This is a test modal body",
+				   		        "This is a test modal body",
+				   		        "This is a test modal body"];
