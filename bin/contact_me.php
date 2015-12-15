f15edb484002bf4301156cb24dba7bbbc846b3ab
kelvinafox <kelvin.a.fox@gmail.com>
2015-12-14 18:34:42 -0800

    Content Update

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -16,10 +16,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'ron@bikemasters.net'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'info@bikemasters.net'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Website Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@your-domain.com\n";
+$headers = "From: noreply@bikemasters.net\n";
 $headers .= "Reply-To: $email_address"; 
 mail($to,$email_subject,$email_body,$headers);
 return true;			
