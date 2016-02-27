fd56730660abe52727f30987fb1ea8f82e28f30c
kelvinafox <kelvin.a.fox@gmail.com>
2016-02-26 18:27:43 -0800

    Content Update
    Added content and pictures to closeouts.php.

--- closeouts.php
+++ closeouts.php
@@ -21,26 +21,162 @@
 
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
 
-        <script src="js/content/mtb_content.js"></script>
-
 	</head>
 	<body>
 
 <!-- Navbar wrapper
 ================================================== -->
     <?php include("includes/navbar.php"); ?>
+    <!-- /.Navbar -->
 
-<!-- Bike Content
-================================================== -->
     <div class="container">
 
-        <!-- Giant Brand -->
-        <div class="container">
-            <h2 class="page-header">Giant Off-Road Bikes</h2>
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Bike Closeouts</h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Home</a>
+                    </li>
+                    <li class="active">Bike Closeouts</li>
+                </ol>
+            </div>
         </div>
-        <div id="catalogue" class="container-fluid">
-            <!-- Call JavaScript File to Load Giant Content -->
-            <script src="js/thumb-scripts/giant.js"></script>
+        <!-- /.row -->
+
+        <!-- Yeti SB5-C -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Yeti SB5-C 2015</h1>
+                <img width="1000" height="600" src="pictures/closeouts/yeti_sb5c.jpg">
+                <br>
+                <p>
+                    Frame - Carbon
+                    Shifters - SRAM X.9 2x10
+                    Rear Derailleur - SRAM X.9
+                    Front Derailleur - Shimano SLX
+                    Brakes - SRAM Guide Hydraulic
+                    <br>
+                    Available in Medium
+                    <br><br>
+                    <h4 style="margin:0;">Was $6300</h4>
+                    <h3 style="color:red;">Now $4999</h3>
+                </p>
+                <br>
+                <hr>
+            </div>
+        </div>
+
+        <!-- Kona Wo -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Kona Wo</h1>
+                <img width="1000" height="600" src="pictures/closeouts/kona_wo.jpg">
+                <p>
+                    Fork - Carbon
+                    Frame - Aluminum
+                    Shifters - Shimano Deore 2x10
+                    Rear Derailleur - Shimano SLX Shadow+
+                    Front Derailleur - Shimano Deore
+                    Brakes - Shimano Deore Hydraulic
+                    <br>
+                    Available in Extra-Large
+                    <br><br>
+                    <h4 style="margin:0;">Was $2099</h4>
+                    <h3 style="color:red;">Now $1599</h3>
+                </p>
+                <br>
+                <hr>
+            </div>
+        </div>
+
+        <!-- Giant Defy Advanced 2 - 2015 -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Giant Defy Advanced 2 - 2015</h1>
+                <img width="1000" height="600" src="pictures/closeouts/defy_adv_2.jpg">
+                <p>
+                    Frame - Giant Advanced Grade Carbon Fiber
+                    Shifters - Shimano 105 5800 2x11
+                    Rear Derailleur - Shimano 105 5800 11-Speed
+                    Front Derailleur - Shimano 105 5800
+                    Brakes - TRP Spyre Disc Brakes
+                    Wheels - Giant PR-2 Wheels
+                    <br>
+                    Available in Medium/Large (Not medium and large, but M/L).
+                    <br><br>
+                    <h4 style="margin:0;">Was $2125</h4>
+                    <h3 style="color:red;">Now $1600</h3>
+                </p>
+                <br>
+                <hr>
+            </div>
+        </div>
+
+        <!-- Giant Defy Advanced 3 - 2015 -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Giant Defy Advanced 3 - 2015</h1>
+                <img width="1000" height="600" src="pictures/closeouts/defy_adv_3-black.jpg">
+                <p>
+                    Frame - Giant Advanced Grade Carbon Fiber
+                    Shifters - Shimano Tiagra 4700 2x10
+                    Rear Derailleur - Shimano Tiagra 4700 10-Speed
+                    Front Derailleur - Shimano Tiagra 4700
+                    Brakes - TRP Spyre Disc Brakes
+                    Wheels - Giant SR-2 Wheels
+                    <br>
+                    Available in Medium.
+                    <br><br>
+                    <h4 style="margin:0;">Was $1800</h4>
+                    <h3 style="color:red;">Now $1350</h3>
+                </p>
+                <br>
+                <hr>
+            </div>
+        </div>
+
+        <!-- Giant Defy Advanced 3 - 2015 -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Giant Defy Advanced 3 - 2015</h1>
+                <img width="1000" height="600" src="pictures/closeouts/defy_adv_3-blue.jpg">
+                <p>
+                    Frame - Giant Advanced Grade Carbon Fiber
+                    Shifters - Shimano Tiagra 4700 2x10
+                    Rear Derailleur - Shimano Tiagra 4700 10-Speed
+                    Front Derailleur - Shimano Tiagra 4700
+                    Brakes - TRP Spyre Disc Brakes
+                    Wheels - Giant SR-2 Wheels
+                    <br>
+                    Available in Medium.
+
+                    <br><br>
+                    <h4 style="margin:0;">Was $1800</h4>
+                    <h3 style="color:red;">Now $1350</h3>
+                </p>
+                <br>
+                <hr>
+            </div>
+        </div>
+
+        <!-- Kona Coco -->
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>Kona Coco</h1>
+                <img width="1000" height="600" src="pictures/closeouts/kona_coco.jpg">
+                <p>
+                    Frame - Aluminum
+                    Shifter - Shimano Alivio 9-Speed
+                    Rear Derailleur - Shimano Alivio 9-Speed
+                    Brakes - Tektro Novela Disc Brakes
+                    Includes Fenders
+
+                    <br><br>
+                    <h4 style="margin:0;">Was $699</h4>
+                    <h3 style="color:red;">Now $490</h3>
+                </p>
+            </div>
         </div>
 
     </div>
@@ -49,45 +185,6 @@
 
 <!-- FOOTER -->
 <?php include("includes/footer.html"); ?>
-
-
-<!-- MUST STAY ON THE BOTTOM -->
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-
-        <!-- Modal Scripts -->
-        <script>
-        
-        /* Giant */
-        for (i = 0; i < num_items_giant; i++) {
-            setupButton("btn-giant" + i, "modal-giant" + i);
-        }
-        
-        function setupButton(button, modal){
-            $(document).ready(function(){
-                $("#" + button).click(function(){
-                    $("#" + modal).modal();
-                });
-            });
-        }
-        </script>
-		<script>
-			/* Script waits for an object within the body to be clicked with the class of 'btn'.
-			 * Once that requirement has been met it gets the value of the button as well as the
-			 * id of the button. and then depending on the id of the button, it sends the content
-			 * page what button number and whether it was giant or liv.
-			 */
-			$("body").on('click', '.btn', function(){
-            var buttonVal = $(this).val();
-			var ID = this.id;
-			var buttonID = ID.replace(/[0-9]/g, ''); //Takes the button ID and removes the number at the end for easier string comparison.
-			if (buttonID == "btn-giant") {
-			   $("#giant-dialog" + buttonVal).load('mtb_content.php', {num : buttonVal, id : buttonID})
-            }
-        })
-		</script>
 
 	</body>
 </html>
